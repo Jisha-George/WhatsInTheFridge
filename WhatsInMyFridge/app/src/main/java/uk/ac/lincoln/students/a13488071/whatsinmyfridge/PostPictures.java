20ad3f57e7201dad3ca8822ce6f73b325c321842
Your Name <you@example.com>
2018-12-20 02:41:03 +0000

    Chnages to the AddImages
    After taking an image, the app also displays it

--- WhatsInMyFridge/app/src/main/java/uk/ac/lincoln/students/a13488071/whatsinmyfridge/PostPictures.java
+++ WhatsInMyFridge/app/src/main/java/uk/ac/lincoln/students/a13488071/whatsinmyfridge/PostPictures.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -22,17 +23,15 @@ import java.util.Date;
 
 public class PostPictures extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 0;
     ImageView ImView;
     String mCurrentPhotoPath;
     ContentValues values;
-    Bitmap help1;
-    ThumbnailUtils thumbnail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_pictures);
+        ImView = (ImageView)findViewById(R.id.imageView);
     }
 
     public void takePicture(View view) {
@@ -55,6 +54,24 @@ public class PostPictures extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            try {
+                Bitmap ImageBit = MediaStore.Images.Media.getBitmap(this.getContentResolver(),Uri.parse(mCurrentPhotoPath));
+                ImView.setImageBitmap(ImageBit);
+                Toast.makeText(this, "Picture was saved!", Toast.LENGTH_SHORT).show();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        } else if (requestCode == RESULT_CANCELED) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, "Picture was not taken", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     //this method will create and return the path to the image file
 
     private File getFile() throws IOException {
@@ -65,34 +82,7 @@ public class PostPictures extends AppCompatActivity {
         File image = File.createTempFile(imageFileName, ".jpg", storageDir);
 
         //Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
         return image;
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && requestCode == Activity.RESULT_OK) {
-            if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                ImView.setImageBitmap(imageBitmap);
-            } else if (requestCode == RESULT_CANCELED) {
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
-                Toast.makeText(this, "Picture was not taken", Toast.LENGTH_SHORT);
-
-//                super.onActivityResult(requestCode, resultCode, data);
-//                if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-//                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-//                    ImView.setImageBitmap(bitmap);
-//                } else if (requestCode == RESULT_CANCELED) {
-//                    Intent intent = new Intent(this, MainActivity.class);
-//                    startActivity(intent);
-//                    Toast.makeText(this, "Picture was not taken", Toast.LENGTH_SHORT);
-//                }
-            }
-        }
     }
 }
